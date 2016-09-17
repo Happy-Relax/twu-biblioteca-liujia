522c8dc4c868201d9f6b0a0f315d3970a7924e9b
liujia <jiaLiu@CNjiaLiu.local>
2016-09-17 19:20:46 +0800

    show main menu

--- src/com/twu/biblioteca/MainMenu.java
+++ src/com/twu/biblioteca/MainMenu.java
@@ -1,7 +1,25 @@
 package com.twu.biblioteca;
 
+import sun.jvm.hotspot.oops.CellTypeStateList;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by jiaLiu on 9/17/16.
  */
 public class MainMenu {
+
+    private List<String> mainMenu = new ArrayList<>();
+
+    public MainMenu (){
+        this.mainMenu.add("ListBooks");
+    }
+
+    public List<String> GetMainMenu() {
+        return this.mainMenu;
+    }
 }
+
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -4,6 +4,10 @@ package com.twu.biblioteca;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
@@ -18,8 +22,15 @@ public class ExampleTest {
     @Test
     public void Should_show_main_menu_options(){
         MainMenu mainMenu = new MainMenu();
-        assertEquals();
+
+        assertEquals(1, mainMenu.GetMainMenu().size());
+        assertEquals("ListBooks", mainMenu.GetMainMenu().get(0));
     }
 
+    @Test
+    public void Should_show_all_books_when_choose_list_books_option(){
+        
+
+    }
 
 }
